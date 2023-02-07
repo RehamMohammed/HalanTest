d94221fb3ebbfe2bf54255f5eeddaa2778d6f02b
RehamMohammed <38201179+RehamMohammed@users.noreply.github.com>
2023-02-07 03:02:09 +0200

    Problem3 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word");
         String word = sc.nextLine();
-        Problem1 obj = new Problem1();
-        boolean res = obj.isPalindrome(word);
+        Problem1 p1 = new Problem1();
+        boolean res = p1.isPalindrome(word);
         System.out.println(res);
+        ////////////////
+        System.out.println("Enter a string to encode");
+        String str = sc.nextLine();
+        Problem3 p2 = new Problem3();
+        String encodedStr = p2.runLengthEncode(str);
+        System.out.println(encodedStr);
+
+        System.out.println("Enter a string to decode");
+        String str2 = sc.nextLine();
+        String decodedStr = p2.runLengthDecode(str2);
+        System.out.println(decodedStr);
+
+
+
     }
 
 }
